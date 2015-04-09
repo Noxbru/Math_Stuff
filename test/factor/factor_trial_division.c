d1fa18028fbaa4daf027a30f083e9d8f60d64aa7
Bruno Jiménez <brunojimen@gmail.com>
2015-04-09 18:28:26 +0100

    Add some tests for trial division

---
+++ test/factor/factor_trial_division.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <gmp.h>
+
+#include "factor.h"
+
+int main(int argc, const char *argv[])
+{
+    mpz_t number;
+
+    if(argc != 2)
+        return 1;
+
+    mpz_init_set_str(number, argv[1], 0);
+
+    printf("%u\n", trial_division_ui(number));
+
+    return 0;
+}
