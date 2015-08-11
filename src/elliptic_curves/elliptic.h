3635e454a1867cc89e8c940878faed077aa7ed3c
Bruno Jiménez <brunojimen@gmail.com>
2015-08-11 13:59:58 +0200

    elliptic: Add a new API for elliptic points

--- src/elliptic_curves/elliptic.h
+++ src/elliptic_curves/elliptic.h
@@ -24,7 +24,8 @@ typedef struct
 typedef enum
 {
     POINT_AFFINE,
-    POINT_PROJECTIVE
+    POINT_PROJECTIVE,
+    POINT_NULL
 } point_type;
 
 typedef struct
@@ -60,6 +61,125 @@ void static inline elliptic_clear(elliptic_ctx *ctx)
 #if FAT_OBJECTS
     mpz_clears(ctx->aux1, ctx->aux2, ctx->lambda, ctx->nu, NULL);
 #endif
+}
+
+
+void static inline elliptic_point_init(elliptic_point *point)
+{
+    mpz_inits(point->x, point->y, point->z, NULL);
+    point->type = POINT_NULL;
+}
+
+void static inline elliptic_point_init_set(
+        elliptic_point *point1,
+        elliptic_point *point2)
+{
+    mpz_init_set(point1->x, point2->x);
+    mpz_init_set(point1->y, point2->y);
+    mpz_init_set(point1->z, point2->z);
+
+    point1->type = point2->type;
+}
+
+void static inline elliptic_point_init_mpz_affine(
+        elliptic_point *point,
+        mpz_ptr x, mpz_ptr y)
+{
+    mpz_init_set(point->x, x);
+    mpz_init_set(point->y, y);
+
+    point->type = POINT_AFFINE;
+}
+
+void static inline elliptic_point_init_mpz_projective(
+        elliptic_point *point,
+        mpz_ptr x, mpz_ptr y, mpz_ptr z)
+{
+    mpz_init_set(point->x, x);
+    mpz_init_set(point->y, y);
+    mpz_init_set(point->z, z);
+
+    point->type = POINT_PROJECTIVE;
+}
+
+void static inline elliptic_point_init_si_affine(
+        elliptic_point *point,
+        int x, int y)
+{
+    mpz_init_set_si(point->x, x);
+    mpz_init_set_si(point->y, y);
+
+    point->type = POINT_AFFINE;
+}
+
+void static inline elliptic_point_init_si_projective(
+        elliptic_point *point,
+        int x, int y, int z)
+{
+    mpz_init_set_si(point->x, x);
+    mpz_init_set_si(point->y, y);
+    mpz_init_set_si(point->z, z);
+
+    point->type = POINT_PROJECTIVE;
+}
+
+void static inline elliptic_point_set(
+        elliptic_point *point1,
+        elliptic_point *point2)
+{
+    mpz_set(point1->x, point2->x);
+    mpz_set(point1->y, point2->y);
+    mpz_set(point1->z, point2->z);
+
+    point1->type = point2->type;
+}
+
+void static inline elliptic_point_set_mpz_affine(
+        elliptic_point *point,
+        mpz_ptr x, mpz_ptr y)
+{
+    mpz_set(point->x, x);
+    mpz_set(point->y, y);
+
+    point->type = POINT_AFFINE;
+}
+
+void static inline elliptic_point_set_mpz_projective(
+        elliptic_point *point,
+        mpz_ptr x, mpz_ptr y, mpz_ptr z)
+{
+    mpz_set(point->x, x);
+    mpz_set(point->y, y);
+    mpz_set(point->z, z);
+
+    point->type = POINT_PROJECTIVE;
+}
+
+void static inline elliptic_point_set_si_affine(
+        elliptic_point *point,
+        int x, int y)
+{
+    mpz_set_si(point->x, x);
+    mpz_set_si(point->y, y);
+
+    point->type = POINT_AFFINE;
+}
+
+void static inline elliptic_point_set_si_projective(
+        elliptic_point *point,
+        int x, int y, int z)
+{
+    mpz_set_si(point->x, x);
+    mpz_set_si(point->y, y);
+    mpz_set_si(point->z, z);
+
+    point->type = POINT_PROJECTIVE;
+}
+
+void static inline elliptic_point_clear(elliptic_point *point)
+{
+    mpz_clears(point->x, point->y, point->z, NULL);
+    point->type = POINT_NULL;
 }
 
 int elliptic_sum(elliptic_point *p_out,
