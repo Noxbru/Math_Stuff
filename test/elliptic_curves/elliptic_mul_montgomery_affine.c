4a770e47b719eb272579fbb56eeb4643ce2863dc
Bruno Jiménez <brunojimen@gmail.com>
2015-08-14 14:45:23 +0200

    test: Add tests for Montgomery Elliptic Curves

---
+++ test/elliptic_curves/elliptic_mul_montgomery_affine.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <gmp.h>
+
+#include "elliptic.h"
+
+int main(int argc, const char *argv[])
+{
+    unsigned int times;
+    elliptic_context ctx;
+    elliptic_point point;
+
+    if(argc != 7)
+        return 1;
+
+    elliptic_curve_init(&ctx);
+
+    mpz_set_str(ctx.A, argv[1], 0);
+    mpz_set_str(ctx.B, argv[2], 0);
+    mpz_set_str(ctx.m, argv[3], 0);
+    ctx.type = ELLIPTIC_CURVE_MONTGOMERY;
+
+    mpz_init_set_str(point.x, argv[4], 0);
+    mpz_init_set_str(point.y, argv[5], 0);
+    mpz_init(point.z);
+    sscanf(argv[6],"%u",&times);
+    point.type = POINT_AFFINE;
+
+    elliptic_curve_mul_montgomery_affine(&point, &point, times, &ctx);
+
+    gmp_printf("%Zd\t%Zd\n", point.x, point.y);
+
+    return 0;
+}
---
+++ test/elliptic_curves/elliptic_sum_montgomery_affine.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <gmp.h>
+
+#include "elliptic.h"
+
+int main(int argc, const char *argv[])
+{
+    elliptic_context ctx;
+    elliptic_point point1;
+    elliptic_point point2;
+
+    if(argc != 8)
+        return 1;
+
+    elliptic_curve_init(&ctx);
+
+    mpz_set_str(ctx.A, argv[1], 0);
+    mpz_set_str(ctx.B, argv[2], 0);
+    mpz_set_str(ctx.m, argv[3], 0);
+    ctx.type = ELLIPTIC_CURVE_MONTGOMERY;
+
+    mpz_init_set_str(point1.x, argv[4], 0);
+    mpz_init_set_str(point1.y, argv[5], 0);
+    mpz_init_set_str(point2.x, argv[6], 0);
+    mpz_init_set_str(point2.y, argv[7], 0);
+    mpz_init(point1.z);
+    mpz_init(point2.z);
+    point1.type = POINT_AFFINE;
+    point2.type = POINT_AFFINE;
+
+    elliptic_curve_sum_montgomery_affine(&point1, &point1, &point2, &ctx);
+
+    gmp_printf("%Zd\t%Zd\n", point1.x, point1.y);
+
+    return 0;
+}
