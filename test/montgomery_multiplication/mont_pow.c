7dea888c39c3abe5e7e42f61d58c03713e42cb2a
Bruno Jiménez <brunojimen@gmail.com>
2015-09-13 13:02:30 +0100

    test: Add tests for montgomery powers

---
+++ test/montgomery_multiplication/mont_pow.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <gmp.h>
+
+#include "mont.h"
+
+int main(int argc, const char *argv[])
+{
+    mont_ctx mctx;
+    mpz_t in;
+    mpz_t mod;
+    mpz_t out;
+    int times;
+
+    if(argc != 4)
+        return 1;
+
+    mpz_init_set_str(in, argv[1], 0);
+    sscanf(argv[2], "%d", &times);
+    mpz_init_set_str(mod, argv[3], 0);
+    mpz_init(out);
+
+    mont_init(&mctx, mod);
+
+    mont_transform(in, in, &mctx);
+
+    mont_pow(out, in, times, &mctx);
+    mont_inv_transform(out, out, &mctx);
+    gmp_printf("%Zd\n",out);
+
+    return 0;
+}
